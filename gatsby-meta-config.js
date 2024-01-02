9667534686f3caa401bfd447f6a775ff6e53b1e6
DongHo Choe <hamfan524@naver.com>
2024-01-02 18:23:59 +0900

    Fix: velog -> linkedIn

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -19,7 +19,7 @@ module.exports = {
     },
     social: {
       github: `https://github.com/hamfan524`, // `https://github.com/hamfan524`,
-      velog: `https://velog.io/@hamfan524/posts`, // ``,
+      linkedIn: `https://velog.io/@hamfan524/posts`, // ``,
       email: `donghochoi312@gmail.com`, // `donghochoi312@gmail.com`,
     },
   },
